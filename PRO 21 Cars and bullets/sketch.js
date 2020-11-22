662c7938bcc053653e6e43756f68a5d30f4ae0fa
pratyush6984 <71959586+pratyush6984@users.noreply.github.com>
2020-11-22 12:09:27 +0530

    Update sketch.js

--- PRO 21 Cars and bullets/sketch.js
+++ PRO 21 Cars and bullets/sketch.js
@@ -10,41 +10,33 @@ function setup() {
 
 	wall=createSprite(1200, 200, thickness,height/2);   
   	wall.shapeColor=color(80,80,80)
-}
-
- function hasCollided(bullet, wall)
- {
-	 bulletRightEdge=bullet.x +bullet.width;
-	 wallLeftEdge=wall.x;
-		if(bulletRightEdge>=wallLeftEdge)
-		{
-			return true
-		}
-		return false;
- }
-	 if(hasCollided(bullet, wall))
-	 {
-		 bullet.velocityX = 0;
-		 var damage = 0.5 * weight *speed *speed/(thickness *thickness *thickness)
-
-		 if(damage>10)
-		 {
-			 wall.shapeColor = color(255,0,0);
-		 }
-
-		 if(damage<10)
-		 {
-			 wall.shapeColor = color(0,255,0);
-		 }
-	 }
+}	
 
 function draw() {
   background(0);
+  if(hasCollided(bullet, wall))
+	{
+		bullet.velocityX = 0;
+		var damage = 0.5 * weight *speed *speed/(thickness *thickness *thickness)
 
+		if(damage>10)
+		{
+			wall.shapeColor = color(255,0,0);
+		}
 
-  
+		if(damage<10)
+		{
+			wall.shapeColor = color(0,255,0);
+		}
+	}
   drawSprites();
- 
 }
-
-
+function hasCollided(bullet, wall){
+	bulletRightEdge = bullet.x + bullet.width;
+	wallLeftEdge = wall.x;
+	   if(bulletRightEdge >= wallLeftEdge)
+	   {
+		   return true
+	   }
+	   return false;
+}
